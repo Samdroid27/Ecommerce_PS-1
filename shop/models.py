05fcff8a38e6793e89316930ddb0b21cbadf90ac
SHIVAM AGRAWAL <f20190326@hyderabad.bits-pilani.ac.in>
2021-07-01 11:25:25 +0530

    Made intial model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=256)
+    pub_date = models.DateField()
